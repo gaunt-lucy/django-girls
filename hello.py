c67430d696ef894e449069f3170c959c76f1baec
Lucy <gaunt.lucy@gmail.com>
2015-03-21 14:54:33 +0000

    Django Girls App

---
+++ hello.py
@@ -0,0 +1,12 @@
+def discount(age):
+    if age <=21 or age >=65:
+        print('Because you are ' + str(age) + ', you can get money off your travel.')
+        if age <=21:
+            print('You qualify for a youth discount.')
+        if age >=65:
+            print('You qualify for a senior discount.')
+    else:
+        print('No discount for you!')
+age = input('How old are you?')
+age = int(age)
+discount(age)
